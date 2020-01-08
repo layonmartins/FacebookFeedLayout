f42401bf8504d56ff49add4a51de1829a92401a0
Layon Martins Fonseca <layonmartins03@gmail.com>
2020-01-08 17:57:35 -0300

    create an AdapterView

--- app/src/main/java/com/hfad/facebookfeedlayout/MainActivity.java
+++ app/src/main/java/com/hfad/facebookfeedlayout/MainActivity.java
@@ -1,15 +1,93 @@
 package com.hfad.facebookfeedlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PostAdapter postAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.recycler_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        postAdapter = new PostAdapter();
+        rv.setAdapter(postAdapter);
+
+
+    }
+
+    private static class PostViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewUser;
+        private final TextView textViewTime;
+        private final TextView textViewUserName;
+        private final TextView textViewContent;
+        private final ImageView imageViewPost;
+        private final TextView textViewTitle;
+        private final TextView textViewSubTitle;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewUser = itemView.findViewById(R.id.image_view_user);
+            textViewTime = itemView.findViewById(R.id.text_view_time);
+            textViewUserName = itemView.findViewById(R.id.text_view_username);
+            textViewContent = itemView.findViewById(R.id.text_view_content);
+            imageViewPost = itemView.findViewById(R.id.image_view_post);
+            textViewTitle = itemView.findViewById(R.id.text_view_post_title);
+            textViewSubTitle = itemView.findViewById(R.id.text_view_post_subtitle);
+        }
+
+        void bind(Post post){
+            imageViewUser.setImageResource(post.getImageViewUser());
+            textViewTime.setText(post.getTextViewTime());
+            textViewUserName.setText(post.getTextViewUserName());
+            textViewContent.setText(post.getTextViewContent());
+            imageViewPost.setImageResource(post.getImageViewPost());
+            textViewTitle.setText(post.getTextViewTitle());
+            textViewSubTitle.setText(post.getTextViewSubTitle());
+        }
+    }
+
+    private class PostAdapter extends RecyclerView.Adapter<PostViewHolder>{
+
+        private List<Post> posts = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.feed_item, parent, false);
+            return new PostViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+            Post post = posts.get(position);
+            holder.bind(post);
+        }
+
+        @Override
+        public int getItemCount() {
+            return posts.size();
+        }
+
+        public void setPosts(List<Post> post){
+            this.posts = posts;
+        }
     }
 
     private class Post {
